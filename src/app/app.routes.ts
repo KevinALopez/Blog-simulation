0fa890079afd01e2392b220eb52029c0608204bb
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-01 18:12:07 +0100

    add BlogComponent + route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BlogComponent } from './pages/blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'blog',
+    pathMatch: 'full',
+  },
+  {
+    path: 'blog',
+    component: BlogComponent,
+  },
+];
