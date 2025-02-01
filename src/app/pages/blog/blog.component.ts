463627497007bee1432eb256af9feff79e281f61
KevinALopez <005.kevin.ayala@gmail.com>
2025-02-01 18:58:49 +0100

    add blogComponent html + PostCard component

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IPost } from '../../interfaces/ipost.interface';
+import { PostCardComponent } from './post-card/post-card.component';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
+  postsList: IPost[] = [];
 
+  blogsSercive = inject(BlogService);
+
+  ngOnInit() {
+    this.postsList = this.blogsSercive.getAll();
+  }
 }
